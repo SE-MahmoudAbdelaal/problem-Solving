f4618c272eb0e44c2ee773922054b8ef59cfd049
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-05-24 21:51:13 +0300

    Even Numbers

---
+++ CodeForces/Sheet #2 (Loops)/B. Even Numbers/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Solution
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int iterateNumber=scanner.nextInt();
+        for (int i = 1; i <= iterateNumber; i++) {
+            System.out.println(i);
+            
+        }
+    }
+}
